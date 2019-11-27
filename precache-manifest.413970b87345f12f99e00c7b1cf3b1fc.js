a69d0f88f752f578ab8b30649bded9bc36a6d570
Mr-emeka <speak2c.emeka@gmail.com>
2019-11-27 17:32:06 +0100

    Updates

---
+++ precache-manifest.413970b87345f12f99e00c7b1cf3b1fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa9e516d565c51e3dd5978096480340",
+    "url": "/Movie-SPA/index.html"
+  },
+  {
+    "revision": "50ac87c7146bcfea4a63",
+    "url": "/Movie-SPA/static/css/main.7960fc8d.chunk.css"
+  },
+  {
+    "revision": "cc1233dcd6b2b87e81a4",
+    "url": "/Movie-SPA/static/js/2.604ce816.chunk.js"
+  },
+  {
+    "revision": "50ac87c7146bcfea4a63",
+    "url": "/Movie-SPA/static/js/main.06ad4304.chunk.js"
+  },
+  {
+    "revision": "0219e8495b00e8dc35aa",
+    "url": "/Movie-SPA/static/js/runtime-main.de8a20a2.js"
+  }
+]);
